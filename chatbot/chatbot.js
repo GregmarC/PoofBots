64fd47d057e59d629508939795356bdb7b5dd147
Gregmar Cruz <gregr1201@gmail.com>
2020-02-20 23:21:55 -0500

    connect server side and dialog flow

---
+++ chatbot/chatbot.js
@@ -0,0 +1,73 @@
+'use strict'
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+const structjson = require('structjson');
+
+//Code from original version on Udemy, obsolete: 
+//const sessionClient = new dialogflow.SessionsClient();   - variable defined below
+//const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+ 
+const projectId = config.googleProjectID;       
+const sessionId = config.dialogFlowSessionID;
+ 
+//Code from original version on Udemy, obsolete: 
+//const languageCode = config.dialogFlowSessionLanguageCode;
+ 
+const credentials = {
+    client_email: config.googleClientEmail,
+    private_key:
+        config.googlePrivateKey,
+};
+
+
+const sessionClient = new dialogflow.SessionsClient({ projectId, credentials });
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+
+module.exports = {
+    textQuery: async function(text, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: text,
+                    // The language used by the client (en-US)
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+            queryParams: {
+                payload: {
+                    data: parameters
+                }
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses)
+        return responses;
+    },
+
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    // The query to send to the dialogflow agent
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters),
+                    // The language used by the client (en-US)
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses)
+        return responses;
+    },
+
+    handleAction: function(responses){
+        return responses;
+    }
+}
